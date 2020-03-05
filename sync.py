c556465eb83c49a455889c0f3c6efe9a1db6ef06
Ji Lau <lauji2k12@gmail.com>
2020-03-05 09:52:44 -0600

    Read and write to competition_json

--- sync.py
+++ sync.py
@@ -13,7 +13,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SHEET_ID = "1W6nyokTCIyZyuXbHV-zY_VHs0qT4zC2EHUAMUBJwa90"
+SHEET_ID = "1QzDvzIwPzpbcthRg69ZdbWdkDXbd1lOI1mI6f5YFh8Q"
 CREDS_PATH = "credentials/credentials.json"
 SHEET_TOKEN_PATH = "credentials/sheettoken.pickle"
 
@@ -47,6 +47,37 @@ class SyncToSheets:
                 matches.append(json.loads(f.read()))
         matches.sort(key=lambda f: f["matchNumber"])
         return matches
+
+    def match_jsons_to_comp_json(self, comp_code):
+        path = os.getcwd() + "/compdata/"
+        competition_json = {}
+        competition_json["competitionCode"] = comp_code
+        competition_json["matches"] = []
+        for file in os.listdir(path):
+            file_path = path+file
+            if os.path.isfile(file_path) and file.split("_")[0] == comp_code:
+                f = open(file_path)
+                print(file_path)
+                competition_json["matches"].append(json.loads(f.read()))
+        competition_json["matches"].sort(key=lambda f: f["matchNumber"])
+        f = open(f"{path}{comp_code}.json", "w")
+        f.write(json.dumps(competition_json, indent=4))
+        f.close()
+        return competition_json
+
+    def push_competition_data(self, comp_code):
+        competition = self.match_jsons_to_comp_json(comp_code)
+        sheet_info = []
+        for match in competition["matches"]:
+            sheet_info.append([match["teamNumber"], match["matchNumber"], match["alliance"], match["initLine"], match["autoLower"], match["autoOuter"],
+                            match["autoInner"], match["lower"], match["outer"], match["inner"], match["rotation"], match["position"], match["park"],
+                            match["hang"], match["level"], match["disableTime"]])
+        body = {
+            "majorDimension": "ROWS",
+            "values": sheet_info
+        }
+        response = self.sheet.values().update(spreadsheetId=SHEET_ID, range="A2:P", valueInputOption="USER_ENTERED", body=body).execute()
+        return f"SUCCESS: {response}"        
     
     def push_match_data(self, comp_code):
         matches = self.read_match_data(comp_code)
@@ -68,5 +99,6 @@ if len(sys.argv) != 2:
     print("Invalid syntax. The script should be run like: python sync.py COMPCODE")
 else:
     sync = SyncToSheets()
-    response = sync.push_match_data(sys.argv[1])
+    #response = sync.push_match_data(sys.argv[1])
+    response = sync.push_competition_data(sys.argv[1])
     print(response)
